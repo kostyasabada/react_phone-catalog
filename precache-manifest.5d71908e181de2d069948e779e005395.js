9ae2bf4085f49c816d935486dd069d548aaaa85d
Kostya Sabada <kostyasabada@gmail.com>
2020-07-30 21:37:15 +0300

    Updates

---
+++ precache-manifest.5d71908e181de2d069948e779e005395.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "210b1a1732964b42f1bff956ee92b2a0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7ac913ce9d460638ccc4",
+    "url": "./static/css/main.39de08ab.chunk.css"
+  },
+  {
+    "revision": "2dccce6a023ac7bdcd3d",
+    "url": "./static/js/2.75343c50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.75343c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac913ce9d460638ccc4",
+    "url": "./static/js/main.10ce212b.chunk.js"
+  },
+  {
+    "revision": "1610068e25a4810046d0",
+    "url": "./static/js/runtime-main.e1f50cee.js"
+  },
+  {
+    "revision": "482e37260cd688576287f1976e958e0b",
+    "url": "./static/media/Mont-Bold.482e3726.otf"
+  },
+  {
+    "revision": "f0f845588eef439ac6ecc9b6ab42191c",
+    "url": "./static/media/Mont-Regular.f0f84558.otf"
+  },
+  {
+    "revision": "d41a520b1802421f24e21279736e1871",
+    "url": "./static/media/Mont-SemiBold.d41a520b.otf"
+  },
+  {
+    "revision": "c8a5446121f3152382ee24a14922bc33",
+    "url": "./static/media/cart.c8a54461.svg"
+  },
+  {
+    "revision": "ba064880a8e6bce13a241bd23737f156",
+    "url": "./static/media/favorites.ba064880.svg"
+  },
+  {
+    "revision": "6da4f5192e45db4de6412d3569a2a464",
+    "url": "./static/media/footer-up.6da4f519.svg"
+  },
+  {
+    "revision": "b8c80bf45f592919992e52583dec278b",
+    "url": "./static/media/left-arrow-block.b8c80bf4.svg"
+  },
+  {
+    "revision": "5ca5c1f58aa3bcefb2bf88439be8fe51",
+    "url": "./static/media/left-arrow.5ca5c1f5.svg"
+  },
+  {
+    "revision": "e80046fae67b5bcf3feffbc6baac09b9",
+    "url": "./static/media/like.e80046fa.svg"
+  },
+  {
+    "revision": "67be4a4a4d709745ad41a3578d953895",
+    "url": "./static/media/right-arrow.67be4a4a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f96163bf66b4bc18893bcedc683b7c29.js"
+  "./precache-manifest.5d71908e181de2d069948e779e005395.js"
 );
 
 self.addEventListener('message', (event) => {
